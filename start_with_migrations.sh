525c4cc1b43e6a6ec24ba653afa5c8f4ea730696
Jacob Ebey <jacob.ebey@live.com>
2022-02-22 18:43:12 +0700

    try to deploy from github I guess...

--- start_with_migrations.sh
+++ start_with_migrations.sh
@@ -6,8 +6,10 @@ npx prisma migrate deploy
 # Determine which configuration file to use based on region.
 if [ "$FLY_REGION" == "$FLY_PRIMARY_REGION" ]
 then
+  echo "Using primary region configuration"
 	mv /etc/litestream.primary.yml /etc/litestream.yml
 else
+  echo "Using replica region configuration"
 	mv /etc/litestream.replica.yml /etc/litestream.yml
 fi
 
