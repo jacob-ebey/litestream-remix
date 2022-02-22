4233e88d8627b98240980a1c8e191d67102e8ea5
Jacob Ebey <jacob.ebey@live.com>
2022-02-23 02:57:37 +0700

    only run mutatino on db for primary region

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -3,17 +3,17 @@ import type { ActionFunction, HeadersFunction } from "remix";
 import prisma from "~/prisma";
 
 export const action: ActionFunction = async ({ request }) => {
-  let count = (await prisma.post.count()) + 1;
-  await prisma.post.create({
-    data: {
-      title: `Post ${count}`,
-      body: `Post body ${count}`,
-    },
-  });
-
   let headers = new Headers();
   if (process.env.FLY_REGION !== process.env.FLY_PRIMARY_REGION) {
     headers.set("fly-replay", `region=${process.env.FLY_PRIMARY_REGION}`);
+  } else {
+    let count = (await prisma.post.count()) + 1;
+    await prisma.post.create({
+      data: {
+        title: `Post ${count}`,
+        body: `Post body ${count}`,
+      },
+    });
   }
 
   return json(null, { headers });
